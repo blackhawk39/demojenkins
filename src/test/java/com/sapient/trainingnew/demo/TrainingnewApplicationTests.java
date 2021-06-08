4ccc130a87a2fc33b10ed125c0e8c3f9daa3bf77
sourabh thakur <sourabh7146@gmail.com>
2021-06-08 16:43:19 +0530

    jenkTest Upate

--- src/test/java/com/sapient/trainingnew/demo/TrainingnewApplicationTests.java
+++ src/test/java/com/sapient/trainingnew/demo/TrainingnewApplicationTests.java
@@ -1,14 +1,46 @@
 package com.sapient.trainingnew.demo;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TrainingnewApplicationTests {
 
-	@Test
-	void test1() {
-		System.out.println(" test1 Passed");
-	}
+	@BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all test methods");
+    }
+ 
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test method");
+    }
+ 
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test method");
+    }
+ 
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all test methods");
+    }
+	
+    @Test
+    @DisplayName("First test")
+    void firstTest() {
+        System.out.println("First test method");
+    }
+    
+    @Test
+    @DisplayName("Second test")
+    void secondTest() {
+        System.out.println("Second test method");
+    }
 
 }
