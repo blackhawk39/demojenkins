e80143dbf11361daa8ca795051226955fb3d1e41
sourabh thakur <sourabh7146@gmail.com>
2021-06-08 12:07:56 +0530

    first

---
+++ src/main/java/com/sapient/trainingnew/demo/TrainingnewApplication.java
@@ -0,0 +1,13 @@
+package com.sapient.trainingnew.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingnewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapient/trainingnew/demo/TrainingnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.trainingnew.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
